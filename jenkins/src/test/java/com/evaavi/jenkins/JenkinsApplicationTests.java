2d31110a16e8c68d3a01fd9e94155a9d2cd25f97
rishiettem <rushikeshettem@gmail.com>
2023-12-28 12:10:52 +0530

    Test

---
+++ jenkins/src/test/java/com/evaavi/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaavi.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
