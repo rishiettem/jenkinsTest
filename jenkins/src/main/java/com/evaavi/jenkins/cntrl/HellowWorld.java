d66f2db3c5b4588cf9ad3cc7f5ff9260ef85c4eb
rishiettem <rushikeshettem@gmail.com>
2023-12-30 21:57:04 +0530

    test2

--- jenkins/src/main/java/com/evaavi/jenkins/cntrl/HellowWorld.java
+++ jenkins/src/main/java/com/evaavi/jenkins/cntrl/HellowWorld.java
@@ -1,12 +1,13 @@
 package com.evaavi.jenkins.cntrl;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/jenkins")
 public class HellowWorld {
-	 @RequestMapping("/test")
+	@GetMapping("/test")
 	    String hello() {
 	        return "Hello World, Rishi!";
 	    }
