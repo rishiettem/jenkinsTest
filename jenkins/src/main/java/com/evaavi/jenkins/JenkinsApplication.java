9139cb886cccab5d47d0a9381a2f913d0b9ac3a4
rishiettem <rushikeshettem@gmail.com>
2023-12-30 23:39:53 +0530

    test5

--- jenkins/src/main/java/com/evaavi/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/evaavi/jenkins/JenkinsApplication.java
@@ -2,10 +2,12 @@ package com.evaavi.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class JenkinsApplication extends SpringBootServletInitializer  {
 
 	
 	public static void main(String[] args) {
@@ -13,4 +15,8 @@ public class JenkinsApplication {
 		System.out.println("Started");
 	}
 
+	 @Override
+	  protected SpringApplicationBuilder  configure(SpringApplicationBuilder builder) {
+	      return builder.sources(JenkinsApplication.class);
+	  }
 }
