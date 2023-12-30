ebd5e2f29fcee00e65aa457229dbf3e6d4990aa5
rishiettem <rushikeshettem@gmail.com>
2023-12-30 22:57:50 +0530

    test4

--- jenkins/src/main/java/com/evaavi/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/evaavi/jenkins/JenkinsApplication.java
@@ -3,11 +3,14 @@ package com.evaavi.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JenkinsApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
+		System.out.println("Started");
 	}
 
 }
