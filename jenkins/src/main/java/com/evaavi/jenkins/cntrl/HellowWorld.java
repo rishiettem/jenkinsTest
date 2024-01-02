9db1b10ba37da4b4f04179df8a3120c522fe5598
rishiettem <rushikeshettem@gmail.com>
2024-01-02 12:18:20 +0530

    test 11

--- jenkins/src/main/java/com/evaavi/jenkins/cntrl/HellowWorld.java
+++ jenkins/src/main/java/com/evaavi/jenkins/cntrl/HellowWorld.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellowWorld {
 	@GetMapping("/test")
 	    String hello() {
-	        return "Hello World, Rishi!";
+	        return "Hello World, Rishi as!";
 	    }
 
 }
